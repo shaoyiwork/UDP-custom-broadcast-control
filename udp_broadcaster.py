740fcea3b3754a250fba22ea03b1947a4f61f00a
sanson <sanson.li@ubtrobot.com>
2018-11-21 16:48:18 +0800

    modify some tips

--- udp_broadcaster.py
+++ udp_broadcaster.py
@@ -8,15 +8,14 @@ PORT = 9999
 BUFSIZE = 1024
  
 ADDR = (HOST, PORT)
- 
 udpCliSock = socket(AF_INET, SOCK_DGRAM)
 udpCliSock.bind(('', 0))
 udpCliSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
-    data = raw_input('>')
+    data = raw_input('please input cmd >')
     if not data:
         break
-    print "sending -> %s"%data
+    print "broadcast sending -> %s"%data
     udpCliSock.sendto(data,ADDR)
 
 udpCliSock.close()
